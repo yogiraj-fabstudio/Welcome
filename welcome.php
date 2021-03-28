b379d074421e4900d1f93d9c2e9ac4f3a6171dbd
yogiraj <ommahadev07@gmail.com>
2021-03-28 21:07:38 +0530

    second commit

--- welcome.php
+++ welcome.php
@@ -33,11 +33,11 @@
             <form action="login.php" method="POST">
 
             <h4>SignUp</h4>
-                <div class="form-group">
+                <!-- <div class="form-group">
                     <label for="exampleInputEmail1">Username</label>
                     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="username">
                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
+                </div> -->
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
